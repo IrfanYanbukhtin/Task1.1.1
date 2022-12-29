d193e4b343814055ed5afa1085a94fe4d732f82b
Irfan Yanbukhtin <woolfyko@gmail.com>
2022-12-29 19:12:12 +0400

    task1.1

---
+++ task.1.1.1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace task._1._1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            if (n >= 1 && n <= 1188)
+            {
+                int il = n / 12;
+                int ay = n % 12;
+                Console.WriteLine($"{n} ay = {il} il ve {ay} ay");
+            }
+            else
+                Console.WriteLine("eded 1-1188 intervalina deyil");
+        }
+    }
+}
